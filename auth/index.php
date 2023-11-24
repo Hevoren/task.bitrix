748f676274ce3b79eb20466a2279f19415b957e8
Hevoren <fifaultimateamfifa15@gmail.com>
2023-11-24 17:33:57 +0300

    [created] cms Bitrix, [added] registration, authorization functionality

---
+++ auth/index.php
@@ -0,0 +1,28 @@
+<?
+define("NEED_AUTH", true);
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+
+if (isset($_REQUEST["backurl"]) && strlen($_REQUEST["backurl"])>0) 
+	LocalRedirect($backurl);
+
+$APPLICATION->SetTitle("Авторизация");
+?><p>
+	 <?$APPLICATION->IncludeComponent(
+	"bitrix:system.auth.form",
+	"",
+	Array(
+		"FORGOT_PASSWORD_URL" => "/auth/forget.php",
+		"PROFILE_URL" => "/auth/personal.php",
+		"REGISTER_URL" => "/auth/login.php",
+		"SHOW_ERRORS" => "N"
+	)
+);?><?$APPLICATION->IncludeComponent(
+	"bitrix:system.auth.confirmation",
+	"",
+	Array(
+		"CONFIRM_CODE" => "confirm_code",
+		"LOGIN" => "login",
+		"USER_ID" => "confirm_user_id"
+	)
+);?><br>
+</p><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
---
+++ main/index.php
@@ -0,0 +1,4 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Title");
+?>Добро пожаловать на сайт<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
